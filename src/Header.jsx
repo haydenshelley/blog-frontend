f256ea5075824d7bf9f11fb00c772c1413883162
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-08-31 10:32:26 -0700

    add navbar to header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,41 @@
 export function Header() {
   return (
-    <header>
-      <a href="#">Home</a> | <a href="#posts-index">All posts</a> |{" "}
-      <a href="#posts-new">New post</a>
-    </header>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Blog
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#posts-new">
+                New Post
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#posts-index">
+                Add Post
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
