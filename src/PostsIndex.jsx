c56d3fdcbce150b736e79b0f62e5969b28d84932
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-08-31 11:59:33 -0700

    add cards to posts index

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -5,18 +5,20 @@ export function PostsIndex(props) {
       <h1>All Posts</h1>
       {props.posts.map((post) => (
         <div key={post.id} className="posts">
-          <h2>{post.title}</h2>
-          <img src={post.image} alt="" />
-          <br />
-          <br />
-          <button
-            onClick={() => props.onShowPost(post)}
-            type="button"
-            className="btn btn-secondary"
-          >
-            More Info
-          </button>
-          <hr />
+          <div className="card" style={{ width: "18rem" }}>
+            <img src={post.image} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">{post.title}</h5>
+              <button
+                onClick={() => props.onShowPost(post)}
+                type="button"
+                classNameName="btn btn-secondary"
+              >
+                More Info
+              </button>
+            </div>
+          </div>
+          ;
         </div>
       ))}
     </div>
--- src/PostsNew.jsx
+++ src/PostsNew.jsx
@@ -2,25 +2,42 @@ export function PostsNew() {
   return (
     <div id="posts-new">
       <h1>New post</h1>
-      <form>
-        <p>
-          <label>Title: </label>
-          <input />
-        </p>
-        <p>
-          <label>Body: </label>
-          <input />
-        </p>
-        <p>
-          <label>Image: </label>
-          <input />
-        </p>
-        <p>
-          <button type="submit" className="btn btn-secondary">
-            Submit
-          </button>
-        </p>
-      </form>
+      <div className="mb-3">
+        <label for="formGroupExampleInput" className="form-label">
+          Title
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="formGroupExampleInput"
+          placeholder="Title..."
+        />
+      </div>
+      <div className="mb-3">
+        <label for="formGroupExampleInput2" className="form-label">
+          Body
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="formGroupExampleInput2"
+          placeholder="Body..."
+        />
+      </div>
+      <div className="mb-3">
+        <label for="formGroupExampleInput2" className="form-label">
+          Image
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="formGroupExampleInput2"
+          placeholder="Image Url..."
+        />
+      </div>
+      <button type="submit" className="btn btn-secondary">
+        Submit
+      </button>
       <hr />
     </div>
   );
