ae952f23a630f7254d0b7ab2703c20878d3f2d78
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-09-01 14:15:17 -0700

    make cards 2 by 2 in posts index

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -3,24 +3,31 @@ export function PostsIndex(props) {
   return (
     <div id="posts-index">
       <h1>All Posts</h1>
-      {props.posts.map((post) => (
-        <div key={post.id} className="posts">
-          <div className="card" style={{ width: "18rem" }}>
-            <img src={post.image} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">{post.title}</h5>
-              <button
-                onClick={() => props.onShowPost(post)}
-                type="button"
-                classNameName="btn btn-secondary"
-              >
-                More Info
-              </button>
+      <div className="container-fluid">
+        <div className="row">
+          {props.posts.map((post) => (
+            <div
+              key={post.id}
+              className="posts d-flex col-6 align-items-stretch"
+            >
+              <div className="card" style={{ width: "18rem" }}>
+                <img src={post.image} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{post.title}</h5>
+                  <button
+                    onClick={() => props.onShowPost(post)}
+                    type="button"
+                    className="btn btn-secondary"
+                  >
+                    More Info
+                  </button>
+                </div>
+              </div>
+              <br />
             </div>
-          </div>
-          <br />
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 }
