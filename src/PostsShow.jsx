74070e8d726bbb0084ccb8797510fe1e4aa976cb
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-09-01 12:29:14 -0700

    add post patch to post modal

--- src/PostsShow.jsx
+++ src/PostsShow.jsx
@@ -1,9 +1,60 @@
+import axios from "axios";
+
 export function PostsShow(props) {
-  console.log(props);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    axios
+      .patch(`http://localhost:3000/posts/${props.post.id}.json`, params)
+      .then((response) => {
+        console.log(response.data);
+      });
+  };
   return (
     <div>
       <h2> {props.post.title} </h2>
       <p> {props.post.body} </p>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Title
+          </label>
+          <input
+            type="text"
+            name="title"
+            className="form-control"
+            id="title-input"
+            defaultValue={props.post.title}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="body" className="form-label">
+            Body
+          </label>
+          <textarea
+            type="text"
+            name="body"
+            className="form-control"
+            id="body-input"
+            defaultValue={props.post.body}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="image" className="form-label">
+            Image
+          </label>
+          <input
+            type="text"
+            name="image"
+            className="form-control"
+            id="image-input"
+            defaultValue={props.post.image}
+          />
+        </div>
+        <button type="submit" className="btn btn-secondary">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
